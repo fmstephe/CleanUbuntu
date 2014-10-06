675315f036743646be7ff080c3ffafc0f9307fec
Francis Stephens <francisstephens@gmail.com>
2014-10-06 17:04:24 +0100

    Now installing xmonad and dmenu

--- clean.sh
+++ clean.sh
@@ -3,5 +3,8 @@
 apt-get --assume-yes remove libreoffice-*
 apt-get --assume-yes remove unity-webapps-*
 
+apt-get --assume-yes install dmenu
+apt-get --assume-yes install xmonad
+
 apt-get autoclean
 apt-get autoremove
