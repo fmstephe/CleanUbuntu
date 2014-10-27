6a1cea674a7710ca0da15b66d99d7049f97039c1
francis stephens <francisstephens@gmail.com>
2014-10-27 14:17:40 +0000

    Removing shotwell, thunderbird, rhythmbox, installing vim

--- clean.sh
+++ clean.sh
@@ -2,7 +2,11 @@
 
 apt-get --assume-yes remove libreoffice-*
 apt-get --assume-yes remove unity-webapps-*
+apt-get --assume-yes remove shotwell
+apt-get --assume-yes remove thunderbird
+apt-get --assume-yes remove rhythmbox
 
+apt-get --assume-yes install vim
 apt-get --assume-yes install dmenu
 apt-get --assume-yes install xmonad
 
