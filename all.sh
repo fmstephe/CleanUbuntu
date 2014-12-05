8eab15ab4c67c9a630eb6c00534ce295defba8dc
Francis Stephens <francisstephens@gmail.com>
2014-12-05 10:13:18 +0000

    Adding comments to all.sh

--- all.sh
+++ all.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Remove garbage I don't want
 apt-get --assume-yes remove libreoffice-*
 apt-get --assume-yes remove unity-webapps-*
 apt-get --assume-yes remove shotwell
 apt-get --assume-yes remove thunderbird
 apt-get --assume-yes remove rhythmbox
 
+# Install marvellous things that I love
 apt-get --assume-yes install vim
 apt-get --assume-yes install tree
 
+# Perform bureaucratic tasks
 apt-get autoclean
 apt-get autoremove
